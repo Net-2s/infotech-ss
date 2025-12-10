547f65e06d54841f7ae0e66873552563ffe1635a
emmanuel <emmanuelpondi07@gmail.com>
2025-12-10 11:30:17 +0100

    first commit

---
+++ infotech/src/test/java/com/n2s/infotech/InfotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n2s.infotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
